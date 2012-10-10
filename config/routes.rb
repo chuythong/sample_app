2534dfec47a15ce786f1da1896e258423c6a1075
Chuy Thong <chuythong_liv@yahoo.com>
2012-10-10 17:30:02 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
+  # URI				Action  Named route				Purpose
+  # ---				------	-----------				-------
+  # /users			index	users_path				page to list all users
+  # /users/1		show	user_path(user)			page to show user
+  #	/users/new		new		new_user_path			page to make a new user (signup)
+  #	/users			create	users_path				create a new user
+  #	/users/1/edit	edit	edit_user_path(user)	page to edit user with id 1
+  #	/users/1		update	user_path(user)			update user
+  #	/users/1		destroy	user_path(user)			delete user
 
   root to: "static_pages#home"
 
@@ -11,4 +21,5 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
+  
 end
